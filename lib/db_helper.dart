488e30d8cf03ed040e94a80952b1adc752e386f9
bhanusunrise <pasindubhanukagood@gmail.com>
2025-01-21 23:40:46 +0530

    DB helper created

---
+++ lib/db_helper.dart
@@ -0,0 +1,55 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'user.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._instance();
+  static Database? _database;
+
+  DatabaseHelper._instance();
+
+  Future<Database> get db async {
+    _database ??= await initDb();
+    return _database!;
+  }
+
+  Future<Database> initDb() async {
+    String databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'pasindu.db');
+
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_display_name VARCHAR,
+        user_email VARCHAR,
+        user_company_code VARCHAR,
+        user_employee_code VARCHAR
+      )
+    ''');
+  }
+
+  Future<int> insertUser(User user) async {
+    Database db = await instance.db;
+    return await db.insert('users', user.toMap());
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllUsers() async {
+    Database db = await instance.db;
+    return await db.query('users');
+  }
+
+  Future<int> updateUser(User user) async {
+    Database db = await instance.db;
+    return await db.update('users', user.toMap(), where: 'id = ?', whereArgs: [user.id]);
+  }
+
+  Future<int> deleteUser(int id) async {
+    Database db = await instance.db;
+    return await db.delete('users', where: 'id = ?', whereArgs: [id]);
+  }
+
+}
